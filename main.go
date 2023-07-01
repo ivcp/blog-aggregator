a3993d4ad773ee2a4f03f8b0af921d8f4b7c6ce9
ivica <blizujemars@gmail.com>
2023-07-01 19:13:31 +0200

    part4

--- main.go
+++ main.go
@@ -1,20 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/ivcp/blog-aggregator/internal/database"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in env")
+	}
+	dbURL := os.Getenv("DB_URL")
+	if portString == "" {
+		log.Fatal("DB URL is not found in env")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to database:", err)
+	}
+
+	queries := database.New(conn)
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
+
 	router := chi.NewRouter()
 	router.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
@@ -28,12 +57,8 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/readiness", handlerReadiness)
 	v1Router.Get("/err", handlerError)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	router.Mount("/v01", v1Router)
-
-	portString := os.Getenv("PORT")
-	if portString == "" {
-		log.Fatal("PORT is not found in env")
-	}
 
 	server := &http.Server{
 		Handler: router,
